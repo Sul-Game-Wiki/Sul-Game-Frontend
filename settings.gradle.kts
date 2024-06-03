ae4037995c3b87daf505a5d8d2fd701483799135
daye200 <daye200@naver.com>
2024-06-03 23:10:56 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sul-Game-Frontend-practice1"
+include(":app")
+ 
