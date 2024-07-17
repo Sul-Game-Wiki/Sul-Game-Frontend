7185348f418640348d277d5eb7b97c6d9b391d20
김진현 <qwekimjh@gmail.com>
2024-07-17 13:57:44 +0900

    design(editprofile): 입력형식 변경

--- app/src/main/java/com/example/sul_game_frontend_practice1/EditProfileActivity.kt
+++ app/src/main/java/com/example/sul_game_frontend_practice1/EditProfileActivity.kt
@@ -3,10 +3,6 @@ package com.example.sul_game_frontend_practice1
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.sul_game_frontend_practice1.databinding.ActivityEditProfileBinding
 import com.example.sul_game_frontend_practice1.databinding.ActivityEditProfileImageBinding
 
