12bf7596b5117c4c9d7166165092601e5c88c5d7
hwan04 <73435826+hwan04@users.noreply.github.com>
2024-07-27 22:03:56 +0900

    init(restart):다시시작

--- app/src/main/java/com/example/sul_game_frontend_practice1/data/Comment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sul_game_frontend_practice1.data
-
-import java.time.LocalDateTime
-
-data class Comment(
-    var writer : User,
-    var content : String,
-    var createdAt : LocalDateTime
-)
--- app/src/main/java/com/example/sul_game_frontend_practice1/main/game/Game.kt
+++
@@ -1,6 +0,0 @@
-package com.example.guhaejo.main.game
-
-data class Game(
-    var game_name: String,
-    var heart_cnt: Int,
-)
