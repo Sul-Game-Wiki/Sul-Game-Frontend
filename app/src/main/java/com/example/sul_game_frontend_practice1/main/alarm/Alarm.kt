e7c2dd1875eb977aaf28e6ab1ca28ecda351da98
daye200 <daye200@naver.com>
2024-08-01 17:59:15 +0900

    로컬 변경 사항 커밋

--- app/src/main/java/com/example/sul_game_frontend_practice1/main/alarm/Alarm.kt
+++
@@ -1,7 +0,0 @@
-package com.example.guhaejo.main.notification
-
-data class Alarm(
-    var type: String,
-    var contents: String,
-    var date: String
-)
--- app/src/main/java/com/example/sul_game_frontend_practice1/main/intro/Intro.kt
+++
@@ -1,7 +0,0 @@
-package com.example.guhaejo.main.intro
-
-data class Intro(
-    var intro_name: String,
-    var intro_detail: String,
-    var heart_cnt: Int,
-)
