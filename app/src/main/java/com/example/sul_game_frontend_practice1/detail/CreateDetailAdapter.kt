70f35d980ee305791f2fd96a610b24547e1a40bb
daye200 <daye200@naver.com>
2024-08-08 19:56:27 +0900

    feat(createdetail):createdetail추가 사항들

---
+++ app/src/main/java/com/example/sul_game_frontend_practice1/detail/CreateDetailAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.sul_game_frontend_practice1.detail
+
+class CreateDetailAdapter {
+}
---
+++ app/src/main/java/com/example/sul_game_frontend_practice1/detail/CreateDetailList.kt
@@ -0,0 +1,8 @@
+package com.example.sul_game_frontend_practice1.detail
+
+import android.graphics.drawable.Drawable
+import org.w3c.dom.Comment
+
+data class CreateDetailComment(var createDetailUserName: String,
+                            var createDetailComment: String,
+                            var createDetailPicture: Int?=null)
