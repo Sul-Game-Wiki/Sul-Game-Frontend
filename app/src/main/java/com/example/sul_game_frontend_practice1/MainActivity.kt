ef6f2060311b2dad2b9a8b477a43ff33379df062
김진현 <qwekimjh@gmail.com>
2024-08-12 21:43:02 +0900

    feat(mypage): 마이페이지 버튼 이동 시 화면 전환 추가

--- app/src/main/java/com/example/sul_game_frontend_practice1/MainActivity.kt
+++ app/src/main/java/com/example/sul_game_frontend_practice1/MainActivity.kt
@@ -5,12 +5,26 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 import com.example.sul_game_frontend_practice1.databinding.ActivityMainBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var behavior: BottomSheetBehavior<ConstraintLayout>
+
+    // BottomSheet가 완전히 펼쳐진 상태인지 확인할 수 있는 변수
+    private var isBottomSheetExpanded : Boolean = false
+
+    /* 현재 열려있는 BottomSheet의 Child를 확인할 수 있는 변수
+    0 : 마이페이지, 1 : 내게시글, 2 : 즐겨찾기게시글, 3 : 프로필 수정 */
+    private var displayedChildBottomSheet : Int = 0
+    companion object {
+        const val MYPAGE = 0
+        const val MYPOST = 1
+        const val FAVORPOST = 2
+        const val EDITPROFILE = 3
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,29 +36,83 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // 화면 초기화 시 호출되는 함수
     private fun initEvent() {
+        isBottomSheetExpanded = false
+        setDisplayedChildBottomSheet(MYPAGE)
+
         binding.searchviewMain.setOnClickListener {
-            val intent = Intent(this@MainActivity, SearchActivity::class.java)
-            startActivity(intent)
+            if (!isBottomSheetExpanded) {
+                val intent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(intent)
+            }
         }
 
-        binding.backgroundDimMain.setOnClickListener {  }
     }
 
+    // 화면 초기화 시 호출되는 bottomSheet에서의 이벤트 처리를 위한 함수
     private fun persistentBottomSheetEvent() {
         behavior = BottomSheetBehavior.from(binding.bottomSheetMain)
-        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+        behavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
-                when(newState) {
-                    BottomSheetBehavior.STATE_EXPANDED -> binding.backgroundDimMain.visibility = View.VISIBLE
-                    BottomSheetBehavior.STATE_COLLAPSED -> binding.backgroundDimMain.visibility = View.GONE
+                when (newState) {
+                    // 완전히 펼쳐졌을 때
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        binding.searchviewMain.isClickable = false
+                        isBottomSheetExpanded = true
+                    }
+
+                    // 드래깅 되고있을 때
+                    BottomSheetBehavior.STATE_DRAGGING ->
+                        binding.backgroundDimMain.visibility = View.VISIBLE
+
+                    // 접혀있을 때
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        binding.backgroundDimMain.visibility = View.GONE
+                        binding.searchviewMain.isClickable = true
+                        isBottomSheetExpanded = false
+                    }
                 }
             }
 
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
                 binding.backgroundDimMain.alpha = slideOffset
-            }
+            } })
 
-        })
+
+        // 마이 페이지 처리
+        binding.tvUsernameMypage.text = "구해조"
+        binding.tvUniversityMypage.text = "세종대학교"
+        binding.btnMypostMypage.setOnClickListener { setDisplayedChildBottomSheet(MYPOST) }
+        binding.btnFavorpostMypage.setOnClickListener { setDisplayedChildBottomSheet(FAVORPOST) }
+        binding.btnEditprofileMypage.setOnClickListener { setDisplayedChildBottomSheet(EDITPROFILE) }
+
+        // 프로필 편집 처리
+        binding.btnSumbitEditprofile.setOnClickListener {
+            setDisplayedChildBottomSheet(MYPAGE)
+        }
+
+
     }
+
+    // BottomSheet의 child 설정
+    private fun setDisplayedChildBottomSheet(child: Int){
+        val bottomSheetTitle = arrayListOf<String>("마이 페이지", "내 게시글", "즐겨찾기 게시글", "프로필 편집")
+
+        binding.viewflipperBottomsheet.displayedChild = child
+        binding.tvTitleMypage.text = bottomSheetTitle[child]
+        displayedChildBottomSheet = child
+    }
+
+    // 뒤로가기 버튼 처리
+    override fun onBackPressed() {
+        if(isBottomSheetExpanded && displayedChildBottomSheet > MYPAGE){
+            setDisplayedChildBottomSheet(0)
+        }
+        else{
+            super.onBackPressed()
+        }
+    }
+
 }
--- app/src/main/java/com/example/sul_game_frontend_practice1/SearchActivity.kt
+++ app/src/main/java/com/example/sul_game_frontend_practice1/SearchActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.sul_game_frontend_practice1.databinding.ActivityMainBinding
 import com.example.sul_game_frontend_practice1.databinding.ActivitySearchBinding
+import com.example.sul_game_frontend_practice1.search.CustomSearchView
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
@@ -19,5 +20,20 @@ class SearchActivity : AppCompatActivity() {
         binding.searchviewSearch.setOnBackListener {
             finish()
         }
+    }
+
+    private fun setOnQueryTextListener(){
+        binding.searchviewSearch.setOnQueryTextListener(object : CustomSearchView.OnQueryTextListener{
+            // 검색 버튼 입력 시 호출
+            override fun onQueryTextChange(newText: String): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            // 텍스트 입력 및 수정 시 호출
+            override fun onQueryTextSubmit(query: String): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        } )
     }
 }
