6ece2c9d454c088e834fd56bd8ddfa795bd14960
hwan04 <73435826+hwan04@users.noreply.github.com>
2024-08-22 19:43:40 +0900

    feat(login, start): button click event

--- app/src/main/java/com/example/sul_game_frontend_practice1/StartActivity.kt
+++ app/src/main/java/com/example/sul_game_frontend_practice1/StartActivity.kt
@@ -1,11 +1,21 @@
 package com.example.sul_game_frontend_practice1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sul_game_frontend_practice1.databinding.ActivityLoginBinding
+import com.example.sul_game_frontend_practice1.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLoginStart.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
