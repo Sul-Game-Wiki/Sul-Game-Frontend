a5519acc3d4a9bd5b51d7fafc72d28f2d4023e0e
김진현 <qwekimjh@gmail.com>
2024-08-25 22:43:55 +0900

    feat(retrofit): 통신부분 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,11 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("de.hdodenhof:circleimageview:2.2.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
