4c4ae0abc7377c83787ecca8f0e82bc0e5cbffdf
hwan04 <73435826+hwan04@users.noreply.github.com>
2024-08-25 14:22:47 +0900

    feat(school): xml design

---
+++ app/src/main/java/com/example/sul_game_frontend_practice1/PermissionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sul_game_frontend_practice1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PermissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permission)
+    }
+}
