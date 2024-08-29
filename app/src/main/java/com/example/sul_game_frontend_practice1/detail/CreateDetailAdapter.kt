bbd44ecf1db04ff185301eccc441622dca62e696
daye200 <daye200@naver.com>
2024-08-29 10:54:52 +0900

    feat(recycler):createdetail recyclerview

--- app/src/main/java/com/example/sul_game_frontend_practice1/detail/CreateDetailAdapter.kt
+++ app/src/main/java/com/example/sul_game_frontend_practice1/detail/CreateDetailAdapter.kt
@@ -1,4 +1,8 @@
 package com.example.sul_game_frontend_practice1.detail
 
-class CreateDetailAdapter {
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sul_game_frontend_practice1.databinding.ItemCreateDetailBinding
+
+class CreateDetailAdapter (val binding: ItemCreateDetailBinding):RecyclerView.ViewHolder(binding.root){
+
 }
--- app/src/main/java/com/example/sul_game_frontend_practice1/detail/CreateDetailList.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sul_game_frontend_practice1.detail
-
-import android.graphics.drawable.Drawable
-import org.w3c.dom.Comment
-
-data class CreateDetailComment(var createDetailUserName: String,
-                            var createDetailComment: String,
-                            var createDetailPicture: Int?=null)
