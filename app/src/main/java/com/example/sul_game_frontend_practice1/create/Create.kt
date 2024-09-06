8f399fe3e8cb3d3be9b57ee0d4daffc14e7daa71
daye200 <daye200@naver.com>
2024-09-06 17:33:51 +0900

    feat(사진,동영상):관련기능 추가

--- app/src/main/java/com/example/sul_game_frontend_practice1/create/Create.kt
+++ app/src/main/java/com/example/sul_game_frontend_practice1/create/Create.kt
@@ -1,37 +1,51 @@
 package com.example.sul_game_frontend_practice1.create
 
+import android.content.ContentProvider
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.example.sul_game_frontend_practice1.R
+import androidx.activity.result.ActivityResultLauncher
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
 import com.example.sul_game_frontend_practice1.databinding.ActivityCreateBinding
-import com.example.sul_game_frontend_practice1.databinding.ActivityMainBinding
+import com.example.sul_game_frontend_practice1.databinding.FragmentCreateCreateBinding
+import com.example.sul_game_frontend_practice1.intro.IntroCreateFragment
+import com.google.android.material.chip.Chip
 
 class Create : AppCompatActivity() {
     private lateinit var binding: ActivityCreateBinding
-    private var isIncludeCreateCreateVisible = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val pagerAdapter = CreateFragmentPagerAdapter(this)
+        binding.vpCreate.adapter = pagerAdapter
         binding.btnChangeCreate.setOnClickListener {
-            if(isIncludeCreateCreateVisible){
-                binding.includeOfficialCreate.root.visibility= View.GONE
-                binding.includeCreateCreate.root.visibility = View.VISIBLE
-                binding.btnChangeCreate.text ="창작"
-
-            }else{
-                binding.includeOfficialCreate.root.visibility= View.VISIBLE
-                binding.includeCreateCreate.root.visibility = View.GONE
-                binding.btnChangeCreate.text ="공식"
-
-            }
-            isIncludeCreateCreateVisible=!isIncludeCreateCreateVisible
-
+            val currentItem = binding.vpCreate.currentItem
+            binding.vpCreate.currentItem = if (currentItem == 0) 1 else 0
         }
+
+
     }
 
+    class CreateFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity){
+        override fun getItemCount(): Int = 2 // 0번과 1번, 두 개의 프래그먼트
 
-}
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> CreateCreateFragment()
+                1 -> IntroCreateFragment()
+                else -> throw IllegalStateException("Unexpected position $position")
+            }
+
+        }
+
+
+}}
