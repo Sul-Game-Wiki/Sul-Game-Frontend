33810dd194a90b3181db064babd240b3f4e0973a
lulyulalla <daye200@naver.com>
2024-09-06 17:40:06 +0900

    feat(detailcomment):이게 진짜 파일

---
+++ app/CreateDetailComment.kt
@@ -0,0 +1,7 @@
+package com.example.sul_game_frontend_practice1.detail
+
+data class CreateDetailComment(var createDetailUserName: String,
+                            var createDetailComment: String,
+                            var createDetailPicture: Int?=null
+
+)
